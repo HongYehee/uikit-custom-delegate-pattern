5db88fa28ce843bac3ba1495070d55633722d6ea
yh.hong <ghddpgml@gmail.com>
2024-11-14 14:29:17 +0900

    initial commit

---
+++ MemberList/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  MemberList
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+## 8️⃣ Custom Delegate Pattern 연습을 위한 '회원 정보 관리'
+- MVC Pattern
+- TableView + NavigationBar + TextField
+- Custom Delegate Pattern
+- Notification
+- ImageView - Gesture
+- PickerView - Delegate Pattern
